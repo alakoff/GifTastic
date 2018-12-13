1c9b68b4804dae17328c3e8ffc1959361449ae04
asl01 <alakoff@gmail.com>
2018-12-12 22:59:03 -0500

    Final Code

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # GifTastic Gif Search
 
-This app will allow the user to enter a gif search term and then add 
+The app will load with four pre-defined search topics; cat, dog, skiing
+and golfing. Click on any one of these buttons to search by the button
+topic.
+
+## Add your own topics !
+
+This app will allow the user to enter a gif search term and then add
 a button for that search term.
+
+## Searching for the Gifs
 
 Click on the button to execute the gif search for that term.
 
-Ten still gifs will be returned. The use can then automate the gifs by
+## Animate the Gifs
+
+Ten still gifs will be returned. The user can then automate the gifs by
 clicking on the gifs, to either start or stop the gifs animation.
-
-
--- assets/javascript/giffy.js
+++ assets/javascript/giffy.js
@@ -1,129 +1,141 @@
 //GifTastic Gifs Search
 
+function loadInitialTopics() {
+
+    var initialTopics = ['cat','dog','skiing','golfing'];
+
+    //Display buttons
+    $(".btnBox").css('display', 'block');
+
+    for (i=0;i<initialTopics.length;i++) {
+
+        //Create new button, id=topic, text=topic, store in var btnNew
+        var btnNew = $('<button>').attr('id',initialTopics[i]).attr('class','buttons').text(initialTopics[i]);
+
+        //Appends new button to buttons box div at top of page
+        $('.btnBox').append(btnNew);
+    }
+}  //End loadInitialTopics Function
+
+
 //Add button for button topic
 function addButton() {
 
-    $("#btnAdd").on('click', function() {
+    //Stores entered button term in variable called topic
+    var topic = $('#btnTerm').val();
 
-        //Stores entered button term in variable called topic
-        var topic = $('#btnTerm').val();
+    //Check for empty term before adding button
+    if (topic==="") {
+        alert('Please enter a search term');
+    } else {
 
-        //Check for empty term before adding button
-        if (topic==="") {
-            alert('Please enter a search term');
-        } else {
+        //Display buttons
+        $(".btnBox").css('display', 'block');
 
-            //Display buttons
-            $(".btnBox").css('display', 'block');
+        //Create new button, id=topic, text=topic, store in var btnNew
+        var btnNew = $('<button>').attr('id',topic).attr('class','buttons').text(topic);
 
-            //Create new button, id=topic, text=topic, store in var btnNew
-            var btnNew = $('<button>').attr('id',topic).attr('class','buttons').text(topic);
+        //Appends new button to buttons div at top of page
+        $('.btnBox').append(btnNew);
 
-            //Appends new button to buttons div at top of page
-            $('.btnBox').append(btnNew);
-
-            //Clears current button term
-            $('#btnTerm').val('');
-
-        }
-        //Get Gifs Function
-        getGifs();
-    })
-    
+        //Clears current button term
+        $('#btnTerm').val('');
+    }
 }  //End addButton Function
 
 
-function getGifs() {
+function getGifs(btnId) {
     //Clears current gifs and then retrieves 10 static gifs for whatever
     //button was clicked
-    
-    $(".buttons").on("click", function () {
 
-        //Get button ID for clicked button
-        var btnId = $(this).attr('id');
+    //ApiKey
+    var apiKey = 'HfEhbmgMfKBB9Lc98mmWHQfm8dC6pSMx';
 
-        //ApiKey
-        var apiKey = 'HfEhbmgMfKBB9Lc98mmWHQfm8dC6pSMx';
-
-        //URL query string
-        var urlQuery = 'https://api.giphy.com/v1/gifs/search?q=' +
-        btnId + '&api_key=' + apiKey;
+    //URL query string
+    var urlQuery = 'https://api.giphy.com/v1/gifs/search?q=' +
+    btnId + '&api_key=' + apiKey;
 
 
-        //AJAX call to get giphy images
-         $.ajax({
-            url: urlQuery,
-            method: "GET"
-         }).then(function(response) {
-             
-                 //Clear current gif images
-                 $(".images").empty();
+    //AJAX call to get giphy images
+     $.ajax({
+        url: urlQuery,
+        method: "GET"
+     }).then(function(response) {
 
-                 //Response processing
-                 var results = response.data;
-             
-                 //Loop to get 10 images only
-                 for (var i = 0; i < 10; i++) {
-                     
-                    var gifDiv = $('<div>');
-                    var p = $("<p>").text("Rating: " + results[i].rating);
-                    var gifImage = $("<img>");
-                    gifImage.attr("src", results[i].images.fixed_height_still.url);
-                    gifImage.attr("data-still", results[i].images.fixed_height_still.url);                
-                    gifImage.attr("data-animate", results[i].images.fixed_height.url);
-                    gifImage.attr('data-state','still');
-                    gifImage.attr("class","gifs");
-                    gifDiv.append(p);
-                    gifDiv.append(gifImage);
-                    $(".images").prepend(gifDiv);
-                 }
-             
-             animateGifs();
-            })
-        
-//        animateGifs();  
-        
+         //Clear current gif images
+         $(".images").empty();
+
+         //Response processing
+         var results = response.data;
+
+         //Loop to get 10 images only
+         for (var i = 0; i < 10; i++) {
+
+            var gifDiv = $('<div>');
+            var title = $("<p>").text("Title: " + results[i].title);
+            var p = $("<p>").text("Rating: " + results[i].rating);
+            var gifImage = $("<img>");
+            gifImage.attr("src", results[i].images.fixed_height_still.url);
+            gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+            gifImage.attr("data-animate", results[i].images.fixed_height.url);
+            gifImage.attr('data-state','still');
+            gifImage.attr("class","gifs");
+            gifDiv.append(title);
+            gifDiv.append(p);
+            gifDiv.append(gifImage);
+            $(".images").prepend(gifDiv);
+         }
+
     })
-    
+
 } //End getGifs Function
 
 
-function animateGifs() {
+function animateGifs(gifImage) {
     //Checks the current animation state of the clicked gif image
     //If current data-state is 'still', then update image url to animated url\
-    
-    $(".gifs").on("click", function () {
-        
-        console.log("animate funct")
-           
-        var state = $(this).attr("data-state");
 
-        if (state === "still") {
+    if (gifImage.attr("data-state") === "still") {
 
-            $(this).attr("src", $(this).attr("data-animate"));     
-            $(this).attr("data-state", "animate");
-        }
+        gifImage.attr("src", gifImage.attr("data-animate"));
+        gifImage.attr("data-state", "animate");
 
-        if (state === "animate") {
+    } else {
 
-            $(this).attr("src", $(this).attr("data-still"));     
-            $(this).attr("data-state", "still");
-        }   
+        gifImage.attr("src", gifImage.attr("data-still"));
+        gifImage.attr("data-state", "still");
 
-    })
-    
+    }
 
 }  //End animateGifs Function
 
-        
-$(document).ready(function () {   
-    addButton();
-})
+//Document ready function
+$(document).ready(function () {
 
+    //Loads initial search terms from a fixed array
+    loadInitialTopics();
 
-    
+    //Adds a search term if a new search term is entered
+    $(document).on("click","#btnAdd", function() {
+        addButton();
+    })
 
+    //Get gifs for the topic of the buttion clicked
+    $(document).on("click",".btnBox .buttons", function() {
+        var btnId = $(this).attr('id');
+        getGifs(btnId);
+    })
 
+    //Animate or stop animation of clicked gif image
+    $(document).on("click",".images .gifs", function() {
+        var gifImage = $(this);
+        animateGifs(gifImage);
+    })
 
-    
+    //Clear all search terms and gif images
+    $(document).on("click","#btnClear",function() {
+        $(".btnBox").empty();
+        $(".images").empty();
+    })
 
+})  //End Document Ready
